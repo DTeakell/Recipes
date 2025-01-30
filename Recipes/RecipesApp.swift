fbbe316848a04cefab80ac4213083c1b343a65fb
Dillon Teakell <teakelld@icloud.com>
2025-01-30 10:13:25 -0500

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Dillon Teakell on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
